82c2fc15d6a5c031068581e79c6f7cfa465e6d88
Jakub Mamelski <jakub.mamelski@outlook.com>
2023-01-06 21:40:46 +0100

    switch

--- Chapter5/controlStructures.go
+++ Chapter5/controlStructures.go
@@ -25,4 +25,17 @@ func main() {
 			println(i, "odd")
 		}
 	}
+
+	switch i {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	case 4:
+		fmt.Println("Four")
+	case 5:
+		fmt.Println("Five")
+	}
 }
