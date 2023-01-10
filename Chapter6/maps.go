c58da3490feba90348aa5f519c8ec529ea75af32
Jakub Mamelski <jakub.mamelski@outlook.com>
2023-01-10 16:12:07 +0100

    Maps

---
+++ Chapter6/maps.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	m := make(map[string]int)
+
+	m["key"] = 10
+	m["key"] = 20
+
+	delete(m, "key")
+
+	value, exists := m["key2"]
+	fmt.Println(reflect.TypeOf(m), m["key"])
+	fmt.Println(reflect.TypeOf(m), m["key2"])
+	fmt.Println(value, exists)
+
+	elements := map[string]string{
+		"H":  "Hydrogen",
+		"He": "Helium",
+		"Li": "Lithium",
+		"Be": "Beryllium",
+		"B":  "Boron",
+		"C":  "Carbon",
+		"N":  "Nitrogen",
+		"O":  "Oxygen",
+		"F":  "Fluorine",
+		"Ne": "Neon",
+	}
+
+	if name, ok := elements["Ne"]; ok {
+		fmt.Println(name, ok)
+	} else {
+		fmt.Println("nie ma")
+	}
+
+	elements2 := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name":  "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name":  "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name":  "Fluorine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name":  "Neon",
+			"state": "gas",
+		},
+	}
+
+	if el, ok := elements2["Li"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
+
+}
