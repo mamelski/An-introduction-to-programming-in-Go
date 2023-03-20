ce7c68b05c98a787413fcf1210b1cf186ca8656f
Jakub Mamelski <jakub.mamelski@outlook.com>
2023-03-20 20:39:16 +0100

    Add Min and Max methods to module

--- Chapter_11_Packages/main.go
+++ Chapter_11_Packages/main.go
@@ -6,5 +6,9 @@ import m "golang-book/Chapter_11_Packages/math2"
 func main() {
 	xs := []float64{1, 2, 3, 4}
 	avg := m.Average(xs)
+
 	fmt.Println(avg)
+
+	fmt.Println(m.Min(xs))
+	fmt.Println(m.Max(xs))
 }
--- Chapter_11_Packages/math2/math2.go
+++ Chapter_11_Packages/math2/math2.go
@@ -8,3 +8,23 @@ func Average(xs []float64) float64 {
 	}
 	return total / float64(len(xs))
 }
+
+func Min(xs []float64) float64 {
+	min := xs[0]
+	for _, x := range xs {
+		if x < min {
+			min = x
+		}
+	}
+	return min
+}
+
+func Max(xs []float64) float64 {
+	max := xs[0]
+	for _, x := range xs {
+		if x > max {
+			max = x
+		}
+	}
+	return max
+}
