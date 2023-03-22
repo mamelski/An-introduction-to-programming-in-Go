506abb3c81f4e1c6daf304b4718030a0d48eb4ac
Jakub Mamelski <jakub.mamelski@outlook.com>
2023-03-22 12:37:05 +0100

    Handle case when input is empty

--- Chapter_11_Packages/math2/math2.go
+++ Chapter_11_Packages/math2/math2.go
@@ -2,6 +2,9 @@ package math2
 
 // Finds the average of a series of numbers
 func Average(xs []float64) float64 {
+	if len(xs) == 0 {
+		return 0
+	}
 	total := float64(0)
 	for _, x := range xs {
 		total += x
--- Chapter_11_Packages/math2/math2_test.go
+++ Chapter_11_Packages/math2/math2_test.go
@@ -11,6 +11,7 @@ var tests = []testpair{
 	{[]float64{1, 2}, 1.5},
 	{[]float64{1, 1, 1, 1, 1, 1}, 1},
 	{[]float64{-1, 1}, 0},
+	{[]float64{}, 0},
 }
 
 func TestAverage(t *testing.T) {
