95b36cfa7273d34954e40cfbaf42472af4382158
Jakub Mamelski <jakub.mamelski@outlook.com>
2023-03-23 17:31:21 +0100

    Add more tests

--- Chapter_11_Packages/math2/math2_test.go
+++ Chapter_11_Packages/math2/math2_test.go
@@ -2,12 +2,12 @@ package math2
 
 import "testing"
 
-type testpair struct {
+type averageTestPair struct {
 	values  []float64
 	average float64
 }
 
-var tests = []testpair{
+var averageTests = []averageTestPair{
 	{[]float64{1, 2}, 1.5},
 	{[]float64{1, 1, 1, 1, 1, 1}, 1},
 	{[]float64{-1, 1}, 0},
@@ -15,12 +15,60 @@ var tests = []testpair{
 }
 
 func TestAverage(t *testing.T) {
-	for _, pair := range tests {
+	for _, pair := range averageTests {
 		v := Average(pair.values)
 		if v != pair.average {
 			t.Error(
 				"For", pair.values,
 				"expected", pair.average,
+				"got", v,
+			)
+		}
+	}
+}
+
+type maxTestPair struct {
+	values []float64
+	max    float64
+}
+
+var maxTests = []maxTestPair{
+	{[]float64{1, 2}, 2},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, 1},
+}
+
+func TestMax(t *testing.T) {
+	for _, pair := range maxTests {
+		v := Max(pair.values)
+		if v != pair.max {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.max,
+				"got", v,
+			)
+		}
+	}
+}
+
+type minTestPair struct {
+	values []float64
+	min    float64
+}
+
+var minTests = []minTestPair{
+	{[]float64{1, 2}, 1},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, -1},
+}
+
+func TestMin(t *testing.T) {
+	for _, pair := range minTests {
+		v := Min(pair.values)
+		if v != pair.min {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.min,
 				"got", v,
 			)
 		}
