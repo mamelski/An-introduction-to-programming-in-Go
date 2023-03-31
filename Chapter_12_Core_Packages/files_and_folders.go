0dfa7f1adee41b6135989f338d9a15c884bb7be6
Jakub Mamelski <jakub.mamelski@outlook.com>
2023-03-31 21:10:09 +0200

    CreateFile

--- Chapter_12_Core_Packages/files_and_folders.go
+++ Chapter_12_Core_Packages/files_and_folders.go
@@ -41,6 +41,18 @@ func ReadFile2() {
 	fmt.Println(str)
 }
 
+func CreateFile() {
+	file, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	file.WriteString("I created this")
+}
+
 func main() {
+	CreateFile()
 	ReadFile2()
 }
