a314df62da078f6103583512953194e6269d58ef
Jakub Mamelski <jakub.mamelski@outlook.com>
2023-04-06 11:17:07 +0200

    tcp client and server

---
+++ Chapter_12_Core_Packages/server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"net"
+)
+
+func server() {
+	// listen on a port
+	ln, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		// Accept a connection
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// handle the connection
+		go handleServerConnection(c)
+	}
+}
+
+func handleServerConnection(c net.Conn) {
+	// receive the message
+	var msg string
+	err := gob.NewDecoder(c).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Received", msg)
+	}
+
+	c.Close()
+}
+
+func client() {
+	// connect to the server
+	c, err := net.Dial("tcp", "localhost:9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// send the message
+	msg := "Hello World"
+	fmt.Println("Sending", msg)
+	err = gob.NewEncoder(c).Encode(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c.Close()
+}
+
+func main() {
+	go server()
+	go client()
+
+	var input string
+	fmt.Scanln(&input)
+}
